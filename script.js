a402403da7eaf51db1092b2ba04ce80fae1259a4
Obianuju Onuegbu <obianuju.onuegbu@decagon.dev>
2023-06-26 14:05:10 +0100

    lecture materials

--- script.js
+++ script.js
@@ -1,6 +1,151 @@
-let x = 2;
-let y = 5;
+// let x = 2;
+// let y = 5;
 
-var sum = x+y;
+// var sum = x+y;
 
-console.log(sum);
+// console.log(sum);
+// var a = 2;
+// var b = 5;
+// console.log(a + b);
+
+// let time = 80;
+// if(time > 860 &&  115)
+
+// console.log('Good day');
+
+//  else if(clock >= 15 && 34) 
+// console.log('Hola');
+
+// else
+//  console.log('hiya');  
+
+
+// let girl = 'smart';
+
+// switch(girl) {
+// case 'smart':
+//    console.log('smart wear');   
+//     break;
+ 
+
+//     case 'corperate':
+//     console.log('dress corperate');
+//     break;
+
+//     default:
+//         console.log('byesssss');
+// }
+
+// let decagon = 'java';
+// switch(decagon) {
+//     case 'java' :
+//  console.log('java stack');
+// break;
+
+// case 'Android' :
+// console.log('android stack');
+// break;
+
+// case 'javaScript' :
+// console.log('javaScript stack');
+// break;
+
+// case 'golang' :
+// console.log('golang stack');
+// break;
+
+// default :
+// console.log('developer');
+// }
+
+
+
+// let item = 'provision';
+
+// switch(item){
+//     case 'fashion':
+//    console.log('dress,bag,shoe,heels');
+//    break;
+
+//    case 'provision' :
+//     console.log('cereal,milk,biscuit')
+//    break;
+
+//    case 'toiletries':
+//     console.log('tissue,soap,camphor');
+//     break;
+
+//     default:
+//     console.log('nothing was bought');
+// }
+
+
+// let place = 'place';
+
+// switch(place){
+//     case 'lagos' :
+//         console.log('welcome to Oshodi');
+//         break;
+
+//     case 'Awka' :
+//         console.log('welcome to Eke Awka');
+//         break;
+
+//     case 'Abuja' :
+//         console.log('welcome to Maitama');
+//         break;
+
+//     case 'Benue' :
+//         console.log('welcome to Gboko');
+//         break;
+
+
+//      default:
+//         console.log('You have missed road');
+
+// }
+
+
+
+// for(i = 0; i < 3000; i++){
+//     console.log('Go buy your own charger!!!', i);
+// }
+
+// for(i = 0; i < 65; i++){
+//     if(i % 2 !== 0) console.log(i);
+// }
+
+
+// let i = 0;
+// while(i < 35){
+// (i % 5 !== 0) 
+// i++; 
+// console.log(i); 
+// }
+
+// const lagos = {
+//     place : 'oshodi',
+//     main  : 'ajah',
+//     estate : 'thomas' 
+// }
+
+// for(let main in lagos){
+//     console.log(main);
+// }
+
+
+// const color = ['red', 'green', 'blue', 'black', 'grey,', 'orange'];
+
+// for(let index in color)
+// console.log(color[4]);
+
+
+let i = 0;
+while(i <= 15){
+    if(i === 5){
+    i++;
+   break;
+}
+    coonsole.log(i);
+    
+}
